9b49d2171a6f7ab707349b51e40a59c6b710fd86
Ankita Verma <Ankita Verma@LAPTOP-2B9BFCAM>
2022-03-23 17:30:53 +0530

    Firstsdet31

---
+++ com.crm.vitiger/src/test/java/com/crm/TestCase/CreateCampaignTest.java
@@ -0,0 +1,14 @@
+package com.crm.TestCase;
+
+import org.testng.annotations.Test;
+
+public class CreateCampaignTest {
+
+	
+@Test
+	
+	public void CreateCamp() {
+		System.out.println("Campaign is creatred");
+	}
+}
+
---
+++ com.crm.vitiger/src/test/java/com/crm/TestCase/CreateOrganizationTest.java
@@ -0,0 +1,12 @@
+package com.crm.TestCase;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganizationTest {
+
+	@Test
+	
+	public void CreateOrg() {
+		System.out.println("Organization is creatred");
+	}
+}
